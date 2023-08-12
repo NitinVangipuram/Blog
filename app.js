4452648014b53eafb641395485960f17ce55c606
NitinVangipuram <140617815+NitinVangipuram@users.noreply.github.com>
2023-08-12 18:55:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -259,6 +259,6 @@ app.get("/seper",function(req,res){
     
   });
   
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
